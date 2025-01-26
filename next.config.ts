10094e9e79ee181f7ddd770d46883a1ced0daa17
Abdul-Raheem324 <abdulraheemwork324@gmail.com>
2025-01-26 19:05:46 +0530

    new commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-require('dotenv').config();
+import path from 'path';
+import { NextConfig } from 'next';
+import { Configuration } from 'webpack';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: false,
+  webpack(config: Configuration) {
+    if (config.resolve) {
+      config.resolve.alias = config.resolve.alias || {}; 
+      (config.resolve.alias as { [key: string]: string })['@'] = path.resolve(__dirname, 'src');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
